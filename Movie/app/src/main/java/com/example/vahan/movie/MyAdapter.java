30aba6c15ed11ae5e5a9ca07b850b6f2dc2e71d8
Vahan <vahan.hovhannisyan.1997@gmail.com>
2018-07-24 22:53:56 +0400

    Added Movie List

--- Movie/app/src/main/java/com/example/vahan/movie/MyAdapter.java
+++ Movie/app/src/main/java/com/example/vahan/movie/MyAdapter.java
@@ -1,23 +1,24 @@
 package com.example.vahan.movie;
 
+import android.graphics.Movie;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
+    private List<Movie> mDataSet = new ArrayList();
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public Movie movie;
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            //this.movie = v;
         }
     }
 
@@ -31,9 +32,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
         // create a new view
-        TextView v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_recycler_view, parent, false);
-
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        ...
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
