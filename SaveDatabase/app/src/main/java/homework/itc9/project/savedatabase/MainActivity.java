22aedc27395404e94bb929b070a2cf247c5f5c49
Vahan <vahan.hovhannisyan.1997@gmail.com>
2018-08-02 01:07:43 +0400

    added task

---
+++ SaveDatabase/app/src/main/java/homework/itc9/project/savedatabase/MainActivity.java
@@ -0,0 +1,19 @@
+package homework.itc9.project.savedatabase;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.containerFrameMain,new SaveDatabaseFragment()).commit();
+
+
+
+    }
+}
