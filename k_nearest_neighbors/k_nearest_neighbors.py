13578c2f778c2222d8cd6ef8d0d076ba3dfcfa48
Vahan <vahan.hovhannisyan.1997@gmail.com>
2018-11-12 00:34:15 +0400

    added 'k nearest' final version

--- k_nearest_neighbors/k_nearest_neighbors.py
+++ k_nearest_neighbors/k_nearest_neighbors.py
@@ -1,70 +1,52 @@
-from math import sqrt
-import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import style
+import warnings
+from collections import Counter
+import pandas as pd
+import random
 
-style.use("fivethirtyeight")
-
-data_set = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
-new_feature = [4, 4]
-
-
-def euclidian_distance(point1, point2):
-    if type(point1) is list and type(point2) is list:
-        return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-    return "No list provided, dumbass!"
+style.use('fivethirtyeight')
 
 
-def find_group_with_nearest_neighbors(ds, nf):
-    # nearest_a1 = ds.get('k')[0]
-    # nearest_a2 = ds.get('k')[1]
-    # nearest_b1 = ds.get('r')[0]
-    # nearest_b2 = ds.get('r')[1]
-    last_index_a = ds.get('k').index(ds.get('k')[ds.__len__()])
-    last_index_b = ds.get('r').index(ds.get('r')[ds.__len__()])
-    # euclidian_distance_last_a = euclidian_distance(ds['k'][last_index_a], nf)
-    # euclidian_distance_last_b = euclidian_distance(ds['r'][last_index_b], nf)
-    total_distance_for_a = 0
-    total_distance_for_b = 0
-    for i in ds:
-        for j in range(len(ds[i])):
-            euclidian_distance_current = euclidian_distance(ds[i][j], nf)
-            if i is 'k':
-                print 'current distance a: ', euclidian_distance_current, '\n'
-                total_distance_for_a += euclidian_distance_current
-            elif i is 'r':
-                total_distance_for_b += euclidian_distance_current
-                print 'current distance b: ', euclidian_distance_current, '\n'
-
-                # if euclidian_distance_current <= euclidian_distance_next and i is 'k':
-                #     nearest_a2 = nearest_a1
-                #     nearest_a1 = ds[i][j]
-                # elif euclidian_distance_current <= euclidian_distance_next and i is 'r':
-                #     nearest_b2 = nearest_b1
-                #     nearest_b1 = ds[i][j]
-    # print 'distance last a: ', euclidian_distance_last_a, '\n'
-    # print 'distance last b: ', euclidian_distance_last_b, '\n'
-    predicted_type = None
-    if total_distance_for_a < total_distance_for_b:
-        predicted_type = 'k'
-    elif total_distance_for_b < total_distance_for_a:
-        predicted_type = 'r'
-    print 'Predicted type is: ', predicted_type
-    return predicted_type
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups!')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_distance, group])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    return vote_result
 
 
-# def find_groups(ds, nf):
-#     for i in ds:
-#         for j in range(len(ds[i])):
-#             print 'Item 1 ', three_nearest_neighbors(ds, nf), '.Comparing to ', ds[i][j]
-#             if three_nearest_neighbors(ds, nf).__getitem__(0) is ds[i][j]:
-#                 return i
-#     return None
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace=True)
+df.drop(df.columns[[0]], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
 
+random.shuffle(full_data)
 
-# print 'Predicted group '
-# print find_groups(data_set, new_feature)
-result = find_group_with_nearest_neighbors(data_set, new_feature)
-print 'result is ', result, '\n'
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in data_set[i]] for i in data_set]
-plt.scatter(new_feature[0], new_feature[1], color=result)
-plt._show()
+test_size = 0.2
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
+train_data = full_data[:-int(test_size * len(full_data))]
+test_data = full_data[-int(test_size * len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+print('Accuracy:', float(correct) / total)
