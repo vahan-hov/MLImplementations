b5acd4310557a88315ec570e1ab32b5370173f36
Vahan21 <37872230+Vahan21@users.noreply.github.com>
2019-05-03 00:54:36 +0400

    Update credentials.py

--- StockPredictor/utils/credentials.py
+++ StockPredictor/utils/credentials.py
@@ -1,8 +1,8 @@
 # API key
-api_key = 'uO5EsbrWX1xDQAGCtrXTvm6S4'
+api_key = ''
 # API secret key
-api_secret_key = '7AsOlN9ELnYyL0MwgaH19oM7BmVMwmITQfzzqL0ikrjbutfoe0'
+api_secret_key = ''
 # access token
-access_token = '959579477268365312-0lBX3PXzrneI69yziCwzOjCFAP7i4pJ'
+access_token = ''
 # access secret token
-access_secret_token = 'yn8pzLdPDnole6W6Q6mjtaAd40H4fELDIER0D7oiKV4tG'
+access_secret_token = ''
